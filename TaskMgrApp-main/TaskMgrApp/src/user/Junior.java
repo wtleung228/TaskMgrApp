459162957d4c392f1af24088305a31f9284a6ca9
SIN Pui Keung <130823389+kkkable@users.noreply.github.com>
2024-10-21 14:24:26 +0800

    Delete TaskMgrApp-main directory

--- TaskMgrApp-main/TaskMgrApp/src/user/Junior.java
+++
@@ -1,9 +0,0 @@
-package user;
-
-public class Junior extends User {
-
-	public Junior(String staffName, String username, String pwd, String title) {
-		super(staffName, username, pwd, title);
-	}
-
-}
