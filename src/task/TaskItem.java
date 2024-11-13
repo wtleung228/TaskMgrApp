3ab3322374814ce2e90cf6d66169bdc027384896
herman <shinho5-c@my.cityu.edu.hk>
2024-11-13 16:44:36 +0800

    Add files via upload

---
+++ src/task/TaskItem.java
@@ -0,0 +1,39 @@
+package task;
+
+public class TaskItem {
+	String content;
+	boolean status=false;
+	
+	public TaskItem(String content) {
+		this.content = content;
+	}
+	
+	
+	public TaskItem(String content, boolean status) {
+		this.content = content;
+		this.status = status;
+	}
+	
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public boolean getStatus() {	//return status in boolean "true or false" format
+		return this.status;
+		
+	}
+	
+	public String getStrStatus() {	//return status in "yes or no" format
+		if(status==true) {
+			return "Yes";
+		}else {
+			return "No";
+		}		
+	}
+
+	
+	public String getContent() {
+		return this.content;
+		
+	}
+}
