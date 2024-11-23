6179009fb490ae34ff0bece79a481eb86a4365fa
wtleung228 <wtleung228-c@my.cityu.edu.hk>
2024-11-23 18:18:23 +0800

    added database test case (unit test)

---
+++ src/testing/RoleTest.java
@@ -0,0 +1,16 @@
+package testing;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import user.User;
+import task.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RoleTest {
+	
+}
